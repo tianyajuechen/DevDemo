8ca9ff7f737b227669c3e278c1243fe43115d898
TZY <784627868@qq.com>
2016-11-10 18:49:58 +0800

    mvvm

--- app/src/main/java/com/example/refreshdemo/activity/DataBindingActivity.java
+++ app/src/main/java/com/example/refreshdemo/activity/DataBindingActivity.java
@@ -1,14 +1,22 @@
 package com.example.refreshdemo.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import com.example.refreshdemo.R;
+import com.example.refreshdemo.bean.User;
+import com.example.refreshdemo.databinding.TestBinding;
+import com.example.refreshdemo.presenter.Presenter1;
 
 public class DataBindingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_binding);
+        TestBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding);
+        User user = new User("Tang", "Zhaoyang");
+        binding.setUser(user);
+        Presenter1 p = new Presenter1();
+        binding.setPresenter(p);
     }
 }
